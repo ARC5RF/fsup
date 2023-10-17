d7a4d467099b530bf13c87768e0acbcd29452e0e
ARC5RF <arc5rf@gmail.com>
2023-10-17 16:20:43 -0700

    add From and FromWD

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # fsup
+
 find the first file or path recursively upstream from a base path
+
+# Warning
+
+this does not stop until it finds a match or errors
+
+this will find things WAY higher up the file tree than you may intend, so be careful of file operations outside of your intended scope
+
+this library does not distinguish between folders, files, or any other type of stat... only if the os says it exists
---
+++ fsup.go
@@ -0,0 +1,30 @@
+package fsup
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+func From(base, what string) (string, error) {
+	p := filepath.Join(base, what)
+	if _, err := os.Stat(p); !errors.Is(err, os.ErrNotExist) {
+		if err != nil {
+			return "", err
+		}
+		return p, nil
+	}
+	d := filepath.Dir(base)
+	if len(d) > 0 {
+		return From(d, what)
+	}
+	return "", os.ErrNotExist
+}
+
+func FromWD(what string) (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	return From(wd, what)
+}
